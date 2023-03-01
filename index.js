ba1046dcd01b0fe55e45b5c732f2f8ef49b8edcf
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-03-01 14:25:41 +1000

    chore: wip

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 
-app.get("/circles", async (req, res) => {
+app.get("/meetup", async (req, res) => {
+
+//   query='query { self { id name } }'
+// curl -X POST https://api.meetup.com/gql \
+//   -H 'Authorization: Bearer {YOUR_TOKEN}' \
+//   -H 'Content-Type: application/json' \
+//   -d @- <<EOF
+//     {"query": "$query"}
+// EOF
 
   const config = {
     method: "get",
     url: `https://api.glassfrog.com/api/v3/circles`,
     headers: {
-    	'X-Auth-Token': process.env.TOKEN, 
+    	'X-Auth-Token': process.env.MEETUP_TOKEN, 
     	'Content-Type': 'application/json'
     }
   };
@@ -21,6 +29,31 @@ app.get("/circles", async (req, res) => {
   const response = await axios(config);
   res.send(JSON.stringify(response.data));
 });
+
+app.get("/meetup/photos", async (req, res) => {
+
+  // query($eventId: ID!) {
+  //   event(id: $eventId) {
+  //     id
+  //     images {
+  //       id
+  //       baseUrl
+  //     }
+  //   }
+  // }
+  
+    const config = {
+      method: "get",
+      url: `https://api.glassfrog.com/api/v3/circles`,
+      headers: {
+        'X-Auth-Token': process.env.MEETUP_TOKEN, 
+        'Content-Type': 'application/json'
+      }
+    };
+  
+    const response = await axios(config);
+    res.send(JSON.stringify(response.data));
+  });
 
 app.listen(PORT, err => {
   if (err) console.log(err);
